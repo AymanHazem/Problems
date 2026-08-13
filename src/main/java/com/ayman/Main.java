287fe250afb466dbfd683ee9f81e5515136b7b19
Ayman Hazem <aymooongsr2@gmail.com>
2026-08-13 10:25:08 +0300

    LeetCode 589

--- src/main/java/com/ayman/Main.java
+++ src/main/java/com/ayman/Main.java
@@ -1,22 +1,49 @@
+class Node
+{
+    public int val;
+    public List<Node> children;
+    public Node() {}
+    public Node(int _val) {val = _val;}
+    public Node(int _val, List<Node> _children) {val = _val;children = _children;}
+}
 void main()
 {
-    System.out.println(areAlmostEqual("bank", "kanb"));
+    System.out.println(preorderRecursion(
+            new Node(1, Arrays.asList(new Node(3, Arrays.asList(new Node(5), new Node(6)))
+                    , new Node(2), new Node(4)))));
+    System.out.println(preorder(
+            new Node(1, Arrays.asList(new Node(3, Arrays.asList(new Node(5), new Node(6)))
+                    , new Node(2), new Node(4)))));
 }
-public boolean areAlmostEqual(String s1, String s2)
+// with Recursion
+List<Integer> preorderRecursion(Node root)
 {
-    if (s1.equals(s2))
-        return true;
-    int first = -1, second = -1;
-    int count = 0;
-    for (int i = 0; i < s1.length(); i++)
+    List<Integer> result = new ArrayList<>();
+    traverse(root, result);
+    return result;
+}
+void traverse(Node node, List<Integer> result)
+{
+    if (node == null) return;
+    result.add(node.val);
+    if (node.children != null)
+        for (Node child : node.children) traverse(child, result);
+}
+
+// wit Deque (Faster than Stack)
+List<Integer> preorder(Node root)
+{
+    List<Integer> result = new ArrayList<>();
+    if (root == null) return result;
+    Deque<Node> stack = new ArrayDeque<>();
+
+    stack.push(root);
+    while (!stack.isEmpty())
     {
-        if (s1.charAt(i) != s2.charAt(i))
-        {
-            count++;
-            if (count == 1) first = i;
-            else if (count == 2) second = i;
-            else return false;
-        }
+        Node node = stack.pop();
+        result.add(node.val);
+        if(node.children == null) continue;
+        for (int i = node.children.size() - 1; i >= 0; i--) stack.push(node.children.get(i));
     }
-    return count == 2 && s1.charAt(first) == s2.charAt(second) && s1.charAt(second) == s2.charAt(first);
+    return result;
 }
